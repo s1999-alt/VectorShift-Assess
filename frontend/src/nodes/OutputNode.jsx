e04d565c5b099b0b5e22582cda16d0381a76976a
Siyad Savad <siyadsavad313@gmail.com>
2026-01-17 23:33:02 +0530

    create the outputnode component

---
+++ frontend/src/nodes/OutputNode.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import BaseNode from '../BaseNode';
+
+export const OutputNode = ({ id, data }) => {
+  const [currName, setCurrName] = useState(data?.outputName || 'output');
+  const [outputType, setOutputType] = useState(data.outputType || 'Text');
+
+  const handleNameChange = (e) => {
+    setCurrName(e.target.value);
+  };
+
+  const handleTypeChange = (e) => {
+    setOutputType(e.target.value);
+  };
+
+  return (
+    <BaseNode
+      title="Output"
+      inputs={[{ id: `${id}-value` }]}
+    >
+      <label>
+        Name:
+        <input
+          type="text"
+          value={currName}
+          onChange={handleNameChange}
+        />
+      </label>
+      <label>
+        Type:
+        <select value={outputType} onChange={handleTypeChange}>
+          <option value="Text">Text</option>
+          <option value="File">Image</option>
+        </select>
+      </label>
+    </BaseNode>
+  );
+}
