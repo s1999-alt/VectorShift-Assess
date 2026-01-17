0decba825e35aea911eb950cf697ed2b8c18aba5
Siyad Savad <siyadsavad313@gmail.com>
2026-01-17 17:40:52 +0530

    Input Node

---
+++ frontend/src/nodes/InputNode.jsx
@@ -0,0 +1,31 @@
+// nodes/InputNode.jsx
+import { useState } from "react";
+import BaseNode from "../BaseNode";
+
+export const InputNode = ({ id, data }) => {
+  const [name, setName] = useState(data?.inputName || "input");
+  const [type, setType] = useState(data?.inputType || "Text");
+
+  return (
+    <BaseNode
+      title="Input"
+      outputs={[{ id: `${id}-value` }]}
+    >
+      <label>
+        Name:
+        <input value={name} onChange={(e) => setName(e.target.value)} />
+      </label>
+
+      <label>
+        Type:
+        <select value={type} onChange={(e) => setType(e.target.value)}>
+          <option>Text</option>
+          <option>File</option>
+        </select>
+      </label>
+    </BaseNode>
+  );
+};
+
+
+
